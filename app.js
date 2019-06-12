b5f9e352c0c4b707ea8692fdf5e054fee90171e9
Thiago Giammattey <giammattey@gmail.com>
2019-06-12 17:50:07 -0300

    refactoring

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
-const users = require('./routes/users');
-
-const path = require('path');
+const routes = require('./routes/index');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(users);
-app.get('/', (req, res, next) => {
-	res.sendFile(path.join(__dirname, 'views', 'main.html'));
-});
+
+app.use(routes);
 
 app.listen(3000);
---
+++ public/main.js
@@ -0,0 +1,1 @@
+alert('Rihanna Navy alert!');
